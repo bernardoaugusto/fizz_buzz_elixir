b91a7b5aaff8cf59a956b91e3e14cc0ef7f5409a
Bernardo Augusto Bastos Silva <bernardo.augusto.bastos@gmail.com>
2022-03-20 22:28:15 -0300

    test: when a valid file is provided, returns the converted list and when an invalid file is provided, returns an error

--- test/fizz_buzz_test.exs
+++ test/fizz_buzz_test.exs
@@ -1,8 +1,15 @@
 defmodule FizzBuzzTest do
   use ExUnit.Case
-  doctest FizzBuzz
 
-  test "greets the world" do
-    assert FizzBuzz.hello() == :world
+  describe "build/1" do
+    test "when a valid file is provided, returns the converted list" do
+      expected_response = {:ok, [1, 2, :fizz, 4, :buzz, :buzz, :fizzbuzz, :buzz]}
+      assert FizzBuzz.build("numbers.txt") == expected_response
+    end
+
+    test "when an invalid file is provided, returns an error" do
+      expected_response = {:error, "Error reading the file enoent"}
+      assert FizzBuzz.build("invalid.txt") == expected_response
+    end
   end
 end
