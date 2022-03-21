916bf0c4743fc93d50dcfd7279641a3edd80f6c0
Bernardo Augusto Bastos Silva <bernardo.augusto.bastos@gmail.com>
2022-03-20 22:12:16 -0300

    refactor: refactoring build return

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -3,20 +3,20 @@ defmodule FizzBuzz do
         file_name |> File.read() |> handle_file_read()
     end
 
-    def handle_file_read({:ok, result}) do
-        result |> String.split(",") |> Enum.map(&convert_and_evaluate_numbers/1)
+    defp handle_file_read({:ok, result}) do
+        res = result |> String.split(",") |> Enum.map(&convert_and_evaluate_numbers/1)
+
+        {:ok, res}
     end
 
-    def handle_file_read({:error, reason}), do: "Error reading the file #{reason}"
+    defp handle_file_read({:error, reason}), do: {:error, "Error reading the file #{reason}"}
 
-    def convert_and_evaluate_numbers(elem) do
-      number = String.to_integer(elem)
-
-      evaluate_numbers(number)
+    defp convert_and_evaluate_numbers(elem) do
+      elem |>String.to_integer() |> evaluate_numbers()
     end
 
-    def evaluate_numbers(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :fizzbuzz
-    def evaluate_numbers(number) when rem(number, 3) == 0, do: :fizz
-    def evaluate_numbers(number) when rem(number, 5) == 0, do: :buzz
-    def evaluate_numbers(number), do: number
+    defp evaluate_numbers(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :fizzbuzz
+    defp evaluate_numbers(number) when rem(number, 3) == 0, do: :fizz
+    defp evaluate_numbers(number) when rem(number, 5) == 0, do: :buzz
+    defp evaluate_numbers(number), do: number
 end
